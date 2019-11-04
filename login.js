1679a35907b51726db3d08fb7060a0bfd7d296b7
Havi <havi101@gmail.com>
2019-11-04 11:26:41 +0200

    .

--- login.js
+++ login.js
@@ -4,10 +4,19 @@ let db;
 
 mysql.createPool({
         connectionLimit: 100,
+<<<<<<< HEAD
         host: "localhost",
         user: "root",
         password: "castark91",
         database: "course_matnas"
+=======
+      
+
+        host: "process.env.matnas_course_URL",
+        user: "process.env.matnas_course_USER",
+        password: "process.env.matnas_course_PASSWORD",
+        database: "process.env.matnas_course_DATABASE"
+>>>>>>> 5b1e4a1cbd24ec859042ff7f357721cbdc76a5a2
     })
     .then((c) => {
         db = c;
@@ -18,8 +27,10 @@ mysql.createPool({
 
 module.exports = {
     login
+    // register
 }
 
+<<<<<<< HEAD
 // let users = [{
 //     userName: 'nir',
 //     password: '123'
@@ -39,7 +50,49 @@ function login(res, req) {
         }
 
     }
+=======
+function login(req, res) {
+    debugger;
+    let userName = req.body.userName;
+    let password = req.body.password;
 
-    res.send("ERORR");
-
+    let users;
+    db.query("select * from teachers_workers")
+        .then((u) => {
+            users = u;})
+            .then((users)=>{
+            console.log(users);
+            for (let user of users) {
+                console.log(users);
+                if (user.userName === userName && user.password === password) {
+                    res.send("OK");
+                }
+            }
+            res.status(500);
+            res.send("ERORR");
+        });
 };
+>>>>>>> 5b1e4a1cbd24ec859042ff7f357721cbdc76a5a2
+
+// function register(req, res) {
+//     let userName = req.body.userName;
+//     let password = req.body.password;
+
+//     let users;
+//     db.query("select * from teachers_workers")
+//         .then((u) => {
+//             users = u;
+//             for (let user of users) {
+//                 if (user.userName === userName && user.password === password) {
+//                     res.status(500);
+//                     res.send("ERROR");
+//                 }
+//             }
+//             let user = {
+//                 userName,
+//                 password
+//             };
+//             //??? העברה של המידע לSQL 
+//             db.query("insert into teachers_workers values (" + userName + "," + password + ")")
+//         });
+// }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
@@ -17,6 +18,7 @@ app.use(express.json());
 //user test tamlate
 app.get("/userInfo/:userId", (req, res) => {
     console.log(req.params.userId);
+<<<<<<< HEAD
     res.render("./userInfo", {
         userId: req.params.userId,
 
@@ -30,15 +32,38 @@ app.get("/secretary", (req, res) => {
     console.log(req.params);
     res.render("./pages/secretary", {});
 });
+// login test
+=======
+res.render("./userInfo", {
+    userId: req.params.userId,
 
+});
+});
+app.get("/", (req, res) => {
+console.log(req.params);
+res.render("./pages/home", {});
+});
+app.get("/secretary", (req, res) => {
+console.log(req.params);
+res.render("./pages/secretary", {});
+});
+// login test
+>>>>>>> 5b1e4a1cbd24ec859042ff7f357721cbdc76a5a2
 app.get('/test', (req, res) => {
     return test(req, res);
 });
 
+<<<<<<< HEAD
 app.get('/', (req, res) => res.sendFile('./views/pages/home.ejs', {
     root: __dirname
 }));
 app.get('/', (req, res) => res.sendFile('./views/pages/secretary.ejs', {
+=======
+app.get('/', (req, res) => res.sendFile('./views/pages/home.ejs', {
+    root: __dirname
+}));
+app.get('/', (req, res) => res.sendFile('./views/pages/secretary.ejs', {
+>>>>>>> 5b1e4a1cbd24ec859042ff7f357721cbdc76a5a2
     root: __dirname
 }));
 app.get('/teacherreg', (req, res) => res.sendFile('./public/pages/teacherreg.html', {
@@ -52,10 +77,10 @@ app.get('/courseDetails', (req, res) => res.sendFile('./public/pages/courseDetai
 }));
 
 app.post('/registration/register', (req, res) => {
-    return registration.register(req, res);
+    registration.register(req, res);
 });
 app.post('/registration/login', (req, res) => {
-    login.login(res, req);
+    login.login(req, res);
 });
 
 app.listen(port, () => console.log('Example app listening on port ' + port));
